2022d8f2102829e98262d87cf91992db34a5da71
bruno0907 <bruno0907@gmail.com>
2021-05-08 22:15:56 -0300

    fix: fixed enviroment variables names

--- src/services/discord.ts
+++ src/services/discord.ts
@@ -7,24 +7,25 @@ interface ApplyProps{
   mainSpec: string;
 }
 
-const { WEBHOOK_ID, WEBHOOK_TOKEN, DASHBOARD_URL, MENTION_CHANNEL } = process.env
+const { WEBHOOK_ID, WEBHOOK_TOKEN, APPLY_URL, CHANNEL_TO_MENTION } = process.env
 
 class DiscordNewApplyService {
   async execute({ charName, className, mainSpec }: ApplyProps){
-
     try {
       const hook = new Discord.WebhookClient(String(WEBHOOK_ID), String(WEBHOOK_TOKEN))
-      await hook.send(`${MENTION_CHANNEL} \nVocê tem um novo apply! \n${charName} - ${className} ${mainSpec} \n${DASHBOARD_URL}${charName}`)
-      
+      await hook.send(`${CHANNEL_TO_MENTION} \nVocê tem um novo apply! \n${charName} - ${className} ${mainSpec} \n${APPLY_URL}${charName}`)
+      return      
     } catch (error) {
       console.log(error.message)
-    }
+      return
+    }    
   }
 }
 
 class DiscordNotifyApplyStatusService{
   async execute(_id: ApplyProps){
     console.log(_id)
+    return
   }
 }
 
